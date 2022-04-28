bccafbaf2f3146542c57886cf6c1a0b7355ee5bf
thumbeti <thumbeti@gmail.com>
2022-04-27 22:31:24 -0500

    Initial Commit.

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+
+List<String> rentalSubModes = ['Daily',
+  'Monthly'];
+
+List<String> weeklyOffDay = ['SUN',
+  'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+const double oneTimeRegistrationFee = 500;
+
+String termsAndConditions = 'I hereby confirm the provided are correct and \n\n'
+    'I will take the responsibility of any issues whatsoever that may arise during the execution of the services by Bha App team.';
